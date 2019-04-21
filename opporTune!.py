baac163db632302640a61cb197a7baacdcaf38e7
Aditi Raghavan <aditi.k.raghavan@gmail.com>
2019-04-21 12:53:30 -0400

    Added recognize and match
    -Need to carry out testing
    - Need to separate into different files

--- opporTune!.py
+++ opporTune!.py
@@ -11,6 +11,8 @@ import librosa as _librosa
 import librosa.display as _display
 import csv #store hashes in csv file which can be loaded as dict
 from collections import defaultdict
+import pyaudio
+import wave
 
 #Change presets of the library to the ones needed for our audio analysis
 #From https://librosa.github.io/librosa/auto_examples/plot_presets.html
@@ -24,9 +26,10 @@ neighborSize = 15
 maxDelTime = 10 
 masterDict = dict()
 songList= list()
-#recognize function
+matchThresh = 10
+songsPlayed = 0
 
-'''
+
 # might make sense to use default dict for speed purposes
 def loadDict(hashCsv):
     #create new csv or load dict as csv
@@ -39,10 +42,9 @@ def loadDict(hashCsv):
             masterDict = {rows[0]:rows[1:] for rows in reader}
     except:
         masterDict = dict()
-        print('No existing file') '''
+        print('No existing file') 
 
-      
-    
+   
 def saveDict(hashCsv, masterDict):
     with open(hashCsv, 'w') as file:
         for hashVal in masterDict:
@@ -60,9 +62,14 @@ def songIndex(song):
     else:
         songList.append(song)
         return len(songList) - 1
-        
-        
-
+    
+'''
+class Song(object):
+   def __init__(self,path):
+        self.songID = os.path.basename(os.path.normpath(song))
+        self.songIndex = songIndex(songID) 
+        self.songArray, self.samplingRate = librosa.load(path)'''
+    
 #Use this as template, if lost /Users/aditiraghavan/Documents/PraNos.mp3
 #Fingerprints the song sample
 def fingerprint(song):
@@ -80,16 +87,14 @@ def fingerprint(song):
     fftFilter= fftFilter.transpose()
     print('transpose 2 finished', fftFilter.shape)
     displaySpectrogram(fftFilter, 'linear',2)
+    #createHash(indexList,15,masterDict,songID)
+    #saveDict('hashSong.csv', masterDict)
+    pass
+    
+'''
     #plt.show()
-    print (indexList)
     timeIdx = list()
     freqIdx = list()
-    createHash(indexList,15,masterDict,songID)
-    saveDict('hashSong.csv', masterDict)
-    return 
-    #need to pull out all values with 1 and store indexes and convert 2 tuples
-    #need to convert to correct value 
-'''
     for elem in indexList:
         timeIdx.append(elem[0])
         freqIdx.append(elem[1])
@@ -143,7 +148,6 @@ def peakArray(arr2D, neighborSize):
                     if maxIndex + startIdx == col: 
                         peaks[row][col] = 1 
                         indexList.append((row,col))
-            
     return peaks, indexList   
  
 def addToDict(hashVal, songTime, mainDict):
@@ -152,7 +156,6 @@ def addToDict(hashVal, songTime, mainDict):
     else:
         masterDict[hashVal] = [songTime]
     pass
- 
            
 def createHash(peakIndex, fanOut, mainDict, songID):
     for i in range(len(peakIndex)):
@@ -168,7 +171,82 @@ def createHash(peakIndex, fanOut, mainDict, songID):
                     songTime = (songID,time1)
                     addToDict(tempKey,songTime, mainDict)
     pass
+    
+## Recognizer
+import pyaudio
+import wave
 
 
+   
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+SAMPLING = 44100
+HOPLEN = 1024
+RECORDSECONDS = 5
+songsPlayed = 0 
+waveOutput = "record.wav"
+
+
+#Taken from https://stackoverflow.com/questions/35344649/reading-input-sound-signal-using-python
+def readMic():
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=SAMPLING, input=True,
+                    frames_per_buffer=HOPLEN)
+    frames = list()
+    
+    for i in range(0, int(SAMPLING / HOPLEN * RECORDSECONDS)):
+        data = stream.read(HOPLEN,exception_on_overflow = False)
+        frames.append(data)
+    
+    print("* done recording")
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    wf = wave.open(waveOutput, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(SAMPLING)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    return waveOutput 
+    
+    
+def recognize():
+    song = readMic()
+    y, sr = librosa.load(song)
+    fftTransform = np.abs(librosa.core.stft(y=y, n_fft = 4096,hop_length = 2048) )
+    fftTransform = librosa.power_to_db(fftTransform, ref = np.max)
+    fftTransform = fftTransform.transpose()
+    fftFilter, indexList = peakArray(fftTransform, neighborSize)
+    fftFilter= fftFilter.transpose()
+    print(indexList,neighborSize, masterDict, matchThresh)
+    SongID, offset = match(indexList,neighborSize, masterDict, matchThresh)
+    return SongID, offset
+
+
+def match(peakIndex, fanOut, mainDict, matchThresh):
+    for i in range(len(peakIndex)):
+        for j in range(fanOut):
+            if i + j < len(peakIndex):
+                freq1 = peakIndex[i][1]
+                time1 = peakIndex[i][0]
+                freq2 = peakIndex[i+j][1]
+                time2 = peakIndex[i+j][0]
+                delTime = time2- time1
+                if delTime < maxDelTime and delTime > 0:
+                    tempKey = (hash((delTime,freq1,freq2)))
+                    if tempKey in masterDict:
+                        for value in masterDict[tempKey]:
+                            songID = tuples[0]
+                            time = tuples[1]
+                            offset = time - time1
+                            if songID in count and offset in count[SongID]:
+                                count[songID][offset] += 1 
+                                if count[songID][offset] == threshold:
+                                    return songID,offset
+                            else: 
+                                count[SongID][offset] = 1
+    return None,None
 
 
